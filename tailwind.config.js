01370323aafbac1f1895e8343564a4fc0a8c257e
Sapotong <new.m.tah@gmail.com>
2023-08-26 14:44:30 +0700

    coba pake tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/assets/**/*.{html,js}", "./views/**/*.view.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
